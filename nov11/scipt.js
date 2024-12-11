c3439a60a3899047e19d1c8fde1f9b9b11b6ba6c
pranavpandey02 <pranavpandey479@gmail.com>
2024-12-11 12:55:50 +0530

    commiting nov11

---
+++ nov11/scipt.js
@@ -0,0 +1,5 @@
+// there is four ways to dipaly output
+document.write("heelow ji");
+window.alert("hlw ji kya kr rhe ho");
+console.log("hello world");
+document.getElementById("demo").innerHTML="hello"; 
