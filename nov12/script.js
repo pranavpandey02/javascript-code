fba8cf270885a6de91055f0de6788603d38f175d
pranavpandey02 <pranavpandey479@gmail.com>
2024-12-11 12:57:36 +0530

    nov12 commiting

---
+++ nov12/script.js
@@ -0,0 +1,33 @@
+
+
+// declartion variable;
+
+var name = "John Doe";
+// know the data type se hum data type pata kr skte hai;
+console.log(typeof (name));
+// yadi hume yeh sari cheeze frontend me chahiye ho toh hum yeh krege
+document.write(typeof name);
+
+var age = 30;
+// know the data type se hum data type pata kr skte hai;
+console.log(typeof age);
+// yadi hume yeh sari cheeze frontend me chahiye ho toh hum yeh krege
+document.write(typeof age);
+
+var eligible = true;
+// know the data type se hum data type pata kr skte hai;
+console.log(typeof eligible);
+// yadi hume yeh sari cheeze frontend me chahiye ho toh hum yeh krege
+document.write(typeof eligible);
+
+var data = null;
+// know the data type se hum data type pata kr skte hai;
+console.log(typeof data);
+// yadi hume yeh sari cheeze frontend me chahiye ho toh hum yeh krege
+document.write(typeof data);
+
+// Null ki datatype object ata hai vo javascript ka ek error yadi ni hota toh null hi hota;
+
+// data type----
+
+ 
